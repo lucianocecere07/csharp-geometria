6a177d0f8461d52e0d03aaa1a646952f88a6e79d
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-25 14:32:17 +0100

    chiedere all'utente base e altezza

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,4 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using Geometria;
+
+Rettangolo rettangoloUtente = new Rettangolo();
+
+Console.Write("inserisci la base del rettangolo: ");
+rettangoloUtente.baseRettangolo = int.Parse(Console.ReadLine());
+Console.WriteLine("base: " + rettangoloUtente.baseRettangolo + " cm");
+
+Console.Write("inserisci l'altezza del rettangolo: ");
+rettangoloUtente.altezzaRettangolo = int.Parse(Console.ReadLine());
+Console.WriteLine("altezza: " + rettangoloUtente.altezzaRettangolo + " cm");
+
+int perimetroRettangoloUtente = rettangoloUtente.calcolaPerimetro();
+Console.WriteLine("perimetro: " + perimetroRettangoloUtente + " cm");
+int areaRettangoloUtente = rettangoloUtente.calcolaArea();
+Console.WriteLine("area: " + areaRettangoloUtente + " cm^2");
+
 

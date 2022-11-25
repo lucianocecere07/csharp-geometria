a5129835d11294656b778843be8a6ef8202204c0
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-25 14:16:39 +0100

    classe rettangolo con attributi

---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Rettangolo
+    {
+        //proprietà
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+
+        //metodi
+        public int calcolaArea()
+        {
+            int areaRettangolo = baseRettangolo * altezzaRettangolo;
+            return areaRettangolo;
+        }
+
+        public int calcolaPerimetro()
+        {
+            int perimetroRettangolo = (baseRettangolo + altezzaRettangolo) * 2;
+            return perimetroRettangolo;
+        }
+    }
+}
